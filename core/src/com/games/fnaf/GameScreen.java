2d40de282b60ef2c42adeffbd6f4b447a9338844
Procellmico <Procellmico@users.noreply.github.com>
2014-12-05 20:06:10 +0800

    Started working on the monitor/camera toggle

--- core/src/com/games/fnaf/GameScreen.java
+++ core/src/com/games/fnaf/GameScreen.java
@@ -4,15 +4,18 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 
 public class GameScreen extends ScreenAdapter
 {
 	private Camera camera;
 	private Office office;
 	private boolean lookingAtCamera;
+	private boolean toggling;
 	private SpriteBatch batch;
 	private OrthographicCamera ortho;
 	private Night night;
+	private Rectangle cameraToggleHitbox;
 
 	public GameScreen(SpriteBatch batch)
 	{
@@ -21,6 +24,8 @@ public class GameScreen extends ScreenAdapter
 		this.ortho.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		this.night = Night.NIGHT_1;
 		this.lookingAtCamera = false;
+		this.cameraToggleHitbox = new Rectangle(255f, 27f, Art.cameraToggle.getWidth(), Art.cameraToggle.getHeight());
+		this.toggling = false;
 	}
 
 	@Override
@@ -38,11 +43,18 @@ public class GameScreen extends ScreenAdapter
 		batch.setProjectionMatrix(ortho.combined);
 		ortho.update();
 
-		batch.draw(Art.cameraToggle, 600f, 600f);
+		batch.draw(Art.cameraToggle, 255f, 27f);
 
 		for (Animatronic a : Animatronic.values())
 		{
 			a.getAI().update(a);
+		}
+
+		boolean collision = this.cameraToggleHitbox.contains(Gdx.input.getX(), MathStuff.reverseYCoords(Gdx.input.getY()));
+
+		if (collision && !toggling)
+		{
+			toggling = true;
 		}
 	}
 
