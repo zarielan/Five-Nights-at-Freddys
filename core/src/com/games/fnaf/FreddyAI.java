ec1720a66a1ea96c4bf3c64c2bd67aa583562cd3
Procellmico <Procellmico@users.noreply.github.com>
2014-12-06 23:01:31 +0800

    Still working on Freddy's AI, avoiding conflict with Chica's movement

--- core/src/com/games/fnaf/FreddyAI.java
+++ core/src/com/games/fnaf/FreddyAI.java
@@ -21,9 +21,35 @@ public class FreddyAI extends AI
 		System.out.println(anim.getName() + " is moving!:");
 		System.out.println(anim.getCurrentRoom().getName() + "-> ");
 
+		//The other two (Bonnie and Chica) needs to get off the show stage first before Freddy comes out
 		if (anim.getCurrentRoom() == Room.SHOW_STAGE)
 		{
-			if (Room.SHOW_STAGE.getVisitors()[0])
+			if (Room.SHOW_STAGE.getVisitors()[Animatronic.BONNIE.ordinal()] && !Room.SHOW_STAGE.getVisitors()[Animatronic.CHICA.ordinal()])
+			{
+				anim.setCurrentRoom(Room.DINING_AREA);
+			}
+		}
+		else
+		{
+			Room[] possibleRooms = allowedRooms.get(anim.getCurrentRoom());
+			int chosen = MathUtils.random(0, possibleRooms.length - 1);
+
+			//Checks if Freddy is in the East Hall and will be walking towards the East Hall Corner
+			if (anim.getCurrentRoom() == Room.EAST_HALL && possibleRooms[chosen] == Room.EAST_HALL_CORNER)
+			{
+				//Checks if Chica is in the East Hall Corner...
+				if (Room.EAST_HALL_CORNER.getVisitors()[Animatronic.CHICA.ordinal()])
+				{
+					//Randomly choses who goes
+					boolean freddyMoves = MathUtils.randomBoolean();
+					if (freddyMoves)
+					{
+						Animatronic.CHICA.setCurrentRoom(Room.EAST_HALL);
+					}
+				}
+			}
+
+			anim.setCurrentRoom(possibleRooms[chosen]);
 		}
 
 		System.out.print(anim.getCurrentRoom().getName());
