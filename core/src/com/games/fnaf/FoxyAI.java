8eea466366488c11d6a1c37f8117bc1185470a8c
Procellmico <Procellmico@users.noreply.github.com>
2014-12-08 14:15:45 +0800

    Modified Foxy's AI to prevent conflict with Bonnie

--- core/src/com/games/fnaf/FoxyAI.java
+++ core/src/com/games/fnaf/FoxyAI.java
@@ -1,5 +1,7 @@
 package com.games.fnaf;
 
+import com.badlogic.gdx.math.MathUtils;
+
 public class FoxyAI extends AI
 {
 	private int stage;
@@ -7,7 +9,7 @@ public class FoxyAI extends AI
 	public FoxyAI()
 	{
 		super();
-		stage = 4;
+		stage = 3;
 	}
 
 	public int getStage()
@@ -18,6 +20,42 @@ public class FoxyAI extends AI
 	@Override
 	public void updatePosition(Animatronic anim)
 	{
-		System.out.println(anim.getName());
+		System.out.print(anim.getName() + ": ");
+		System.out.print(anim.getCurrentRoom().getName() + " -> ");
+
+		//If Foxy is no longer at stages 1, 2, or 3, this means he's running. For you.
+		if (stage < 0 || 3 < stage)
+		{
+			//Check if Bonnie is in the West Hall
+			if (Room.WEST_HALL.getVisitors()[Animatronic.BONNIE.ordinal()])
+			{
+				//Move that guy out of the way to either the West Hall Corner, Supply Closet, or Dining Area
+				Room putBonnieIn;
+				switch (MathUtils.random(2))
+				{
+				case 0:
+					putBonnieIn = Room.WEST_HALL_CORNER;
+					break;
+				case 1:
+					putBonnieIn = Room.SUPPLY_CLOSET;
+					break;
+				case 2:
+					putBonnieIn = Room.DINING_AREA;
+					break;
+				default:
+					putBonnieIn = Room.SUPPLY_CLOSET;
+					break;
+				}
+
+				//Clear the way for Foxy!
+				Animatronic.BONNIE.setCurrentRoom(putBonnieIn);
+			}
+
+			//Good luck.
+			anim.setCurrentRoom(Room.WEST_HALL);
+		}
+
+		System.out.print(anim.getCurrentRoom().getName());
+		System.out.println();
 	}
 }
