d8b268a688a0bb8e7f99ccc09b04654cb15eeeb4
Procellmico <Procellmico@users.noreply.github.com>
2014-12-20 22:08:19 +0800

    Made the hitboxes to an array

--- core/src/com/games/fnaf/DoorLights.java
+++ core/src/com/games/fnaf/DoorLights.java
@@ -6,10 +6,11 @@ import com.badlogic.gdx.math.Polygon;
 
 public class DoorLights
 {
-	private final Polygon RIGHT_DOOR_HITBOX;
-	private final Polygon RIGHT_LIGHT_HITBOX;
-	private final Polygon LEFT_DOOR_HITBOX;
-	private final Polygon LEFT_LIGHT_HITBOX;
+	private final int RIGHT_DOOR_HITBOX = 0;
+	private final int RIGHT_LIGHT_HITBOX = 1;
+	private final int LEFT_DOOR_HITBOX = 2;
+	private final int LEFT_LIGHT_HITBOX = 3;
+	private final Polygon[] HITBOXES;
 	private boolean leftDoor;
 	private boolean leftLight;
 	private boolean rightDoor;
@@ -19,10 +20,11 @@ public class DoorLights
 
 	public DoorLights(SpriteBatch batch1)
 	{
-		RIGHT_DOOR_HITBOX = new Polygon(new float[]{1600 - 40 - 160, 350, 1600 - 40 - 160, 402, 1600 - 78 - 160, 400, 1600 - 78 - 160, 348});
-		RIGHT_LIGHT_HITBOX = new Polygon(new float[]{1600 - 38 - 160, 350 - 80, 1600 - 38 - 160, 402 - 80, 1600 - 75 - 160, 401 - 80, 1600 - 75 - 160, 349 - 80});
-		LEFT_DOOR_HITBOX = new Polygon(new float[]{27 - 160, 350, 27 - 160, 403, 67 - 160, 400, 67 - 160, 347});
-		LEFT_LIGHT_HITBOX = new Polygon(new float[]{27 - 160, 350 - 80, 27 - 160, 402 - 80, 67 - 160, 401 - 80, 67 - 160, 349 - 80});
+		HITBOXES = new Polygon[4];
+		HITBOXES[RIGHT_DOOR_HITBOX] = new Polygon(new float[]{1600 - 40 - 160, 350, 1600 - 40 - 160, 402, 1600 - 78 - 160, 400, 1600 - 78 - 160, 348});
+		HITBOXES[RIGHT_LIGHT_HITBOX] = new Polygon(new float[]{1600 - 38 - 160, 350 - 80, 1600 - 38 - 160, 402 - 80, 1600 - 75 - 160, 401 - 80, 1600 - 75 - 160, 349 - 80});
+		HITBOXES[LEFT_DOOR_HITBOX] = new Polygon(new float[]{27 - 160, 350, 27 - 160, 403, 67 - 160, 400, 67 - 160, 347});
+		HITBOXES[LEFT_LIGHT_HITBOX] = new Polygon(new float[]{27 - 160, 350 - 80, 27 - 160, 402 - 80, 67 - 160, 401 - 80, 67 - 160, 349 - 80});
 
 		leftDoor = false;
 		leftLight = false;
