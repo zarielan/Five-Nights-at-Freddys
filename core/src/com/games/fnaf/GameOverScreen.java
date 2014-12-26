fa5f602855cb2c4c132e50f019adc6736467912d
Procellmico <Procellmico@users.noreply.github.com>
2014-12-26 20:31:53 +0800

    Removed some methods and reset X on jumpscare

--- core/src/com/games/fnaf/GameOverScreen.java
+++ core/src/com/games/fnaf/GameOverScreen.java
@@ -1,12 +1,21 @@
 package com.games.fnaf;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameOverScreen extends ScreenAdapter
 {
-	public GameOverScreen()
+	private SpriteBatch batch;
+	private OrthographicCamera camera;
+
+	public GameOverScreen(SpriteBatch batch1)
 	{
 		super();
+		batch = batch1;
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 	}
 
 	@Override
--- core/src/com/games/fnaf/Office.java
+++ core/src/com/games/fnaf/Office.java
@@ -16,7 +16,6 @@ public class Office
 	private boolean[] playDoorAnimation;
 	private boolean[] previousDoorShut;
 	private float[] doorAnimCounter;
-	private boolean resetCameraX;
 
 	public Office(SpriteBatch batch)
 	{
@@ -35,17 +34,10 @@ public class Office
 		playDoorAnimation = new boolean[]{false, false};
 		previousDoorShut = new boolean[]{false, false};
 		doorAnimCounter = new float[]{0f, 0f};
-		resetCameraX = true;
 	}
 
 	public void renderJumpscare(TextureRegion frame)
 	{
-		if (resetCameraX)
-		{
-			camera.position.x = 640f;
-			resetCameraX = false;
-		}
-
 		batch.setProjectionMatrix(camera.combined);
 		camera.update();
 
