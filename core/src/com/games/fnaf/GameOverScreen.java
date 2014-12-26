8c1e61513ef0c13f6407cdb7038b07c89a495477
Procellmico <Procellmico@users.noreply.github.com>
2014-12-26 20:30:09 +0800

    Added initial GameOverScreen

---
+++ core/src/com/games/fnaf/GameOverScreen.java
@@ -0,0 +1,21 @@
+package com.games.fnaf;
+
+import com.badlogic.gdx.ScreenAdapter;
+
+public class GameOverScreen extends ScreenAdapter
+{
+	public GameOverScreen()
+	{
+		super();
+	}
+
+	@Override
+	public void render(float delta)
+	{
+	}
+
+	@Override
+	public void show()
+	{
+	}
+}
